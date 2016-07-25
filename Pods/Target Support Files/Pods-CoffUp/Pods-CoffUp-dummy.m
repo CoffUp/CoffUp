571928cea085f109c9346873b6200461626e0232
brennanMKE <brennan@smallsharptools.com>
2016-07-25 10:15:30 -0700

    updates CocoaPods to 1.0 with new Podfile format

--- Pods/Target Support Files/Pods-CoffUp/Pods-CoffUp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CoffUp : NSObject
-@end
-@implementation PodsDummy_Pods_CoffUp
-@end
--- Pods/Target Support Files/QuadratTouch/QuadratTouch-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "QuadratTouch.h"
-
-FOUNDATION_EXPORT double QuadratTouchVersionNumber;
-FOUNDATION_EXPORT const unsigned char QuadratTouchVersionString[];
-
